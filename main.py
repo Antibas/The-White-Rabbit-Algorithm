e50624c610b3b6b2d1dc2bb27ab71a713334ff93
Antibas <antivasisp@gmail.com>
2025-05-23 16:29:59 +0300

    docs: :memo: Updated main.py and README.md

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import algorithms.dbpedia as dbpedia
 import algorithms.wikidata as wikidata
 import algorithms.yago as yago
 from utils.enums import EmbeddingType
-from utils.utils import timeout
+from utils.utils import load_model, timeout
 from sys import argv
+from argparse import ArgumentParser
+
+parser = ArgumentParser(
+    prog='The White Rabbit',
+    description='Test our algorithm along with various others to make the comparison.',
+    epilog='Outputs the metrics extracted from the selected algorithm.'
+)
 
 DATASETS = {
     "DBPEDIA": dbpedia,
@@ -15,67 +22,51 @@ DATASETS = {
     "3": yago
 }
 
-def usage():
-    print("")
-
-def main(arguments: list[str] = []):
-    if not arguments:
+def main():
+    nodes = []
+    with open("config/nodes.conf") as f:
+        nodes = list(node.strip() for node in f.readlines())
+        nodes.sort()
+    if not argv[1:]:
         print("Welcome to the White Rabbit demo.")
         print("Here you can test our algorithm along with various others to make the comparison.")
         source, target = "", ""
-        with open("config/nodes.conf") as f:
-            nodes = list(node.strip() for node in f.readlines())
-            nodes.sort()
-            entities = "\n\t".join(f"{i+1}. {nodes[i]}" for i in range(len(nodes)))
-            try:
-                sel = input(f"Select two of the following nodes: {entities}\n\nSelect them with their number separated by whitespace: ")
-                selections = tuple(sel.split(" "))
-                s_i, t_i = int(selections[0]), int(selections[1])
-            except IndexError:
-                print(f"Invalid input {sel}. You need to type two numbers")
-                return
-            except ValueError:
-                print(f"One of the inputs is invalid: {selections}")
-                return
+        entities = "\n\t".join(f"{i+1}. {nodes[i]}" for i in range(len(nodes)))
+        try:
+            sel = input(f"\t{entities}\n\nSelect two of the above nodes with their number separated with a whitespace.\nAlternatively you can put your own node names separated with a whitespace: ")
+            selections = tuple(sel.split(" "))
+            s_i, t_i = int(selections[0]), int(selections[1])
             source, target = nodes[s_i-1], nodes[t_i-1]
+        except IndexError:
+            print(f"Invalid input {sel}. You need to type two numbers in [1,60] or string")
+            return
+        except ValueError:
+            source, target = s_i, t_i
         ds = input("Select one of the following datasets:\n\t1. DBPEDIA\n\t2. WIKIDATA\n\t3. YAGO\n\nSelect them with either their number or name: ")
         dataset = None
         try:    
             dataset = DATASETS[ds.upper()]
-        # if(ds.upper() in ["1", "DBPEDIA"]):
-        #     dataset = dbpedia
-        # elif(ds.upper() in ["2", "WIKIDATA"]):
-        #     dataset = wikidata
-        # elif(ds.upper() in ["3", "YAGO"]):
-        #     dataset = yago
-        # else:
         except KeyError as e:
             print(f"Invalid input: {e}")
             return
         
         alg = input("Select one of the following algorithms to test:\n\t1. The White Rabbit\n\t2. Query Expansion\n\t" \
-                    "3. Embeddings\n\t4. LLM\n\nSelect them with either their number or name: ")
+                    "3. Embedding\n\t4. LLM\n\nSelect them with either their number or name: ")
         algorithm = None
         embedding = None
         if(alg.upper() in ["1", "The White Rabbit"]):
             algorithm = dataset.white_rabbit
         elif(alg.upper() in ["2", "Query Expansion"]):
             algorithm = dataset.query_expansion
-        elif(alg.upper() in ["3", "Embeddings"]):
+        elif(alg.upper() in ["3", "Embedding"]):
             algorithm = dataset.embedding
 
             emb = input("Select one of the following embeddings:\n\t1. WORD2VEC\n\t2. FASTTEXT\n\t" \
-                    "3. SBERT\n\nSelect them with either their number or name:")
-            embedding = EmbeddingType(emb)
-            # if(emb.upper() in ["1", "WORD2VEC"]):
-            #     embedding = EmbeddingType(emb)
-            # elif(emb.upper() in ["2", "FASTTEXT"]):
-            #     embedding = EmbeddingType
-            # elif(emb.upper() in ["3", "SBERT"]):
-            #     embedding = EmbeddingType
-            # else:
-            #     print(f"Invalid embedding: {ds}")
-            #     return
+                    "3. SBERT\n\nSelect them with either their number or name: ")
+            try:
+                embedding = EmbeddingType(int(emb) if emb.isnumeric() else emb)
+            except ValueError:
+                embedding = EmbeddingType[emb.upper()]
         elif(alg.upper() in ["4", "LLM"]):
             algorithm = dataset.llm
         else:
@@ -83,49 +74,70 @@ def main(arguments: list[str] = []):
             return
         
         acc = input("(Optional) Select an accuracy threshold between 0 and 0.99: ")
-        try:
-            accuracy_threshold = float(acc)
-            if(accuracy_threshold < 0 or accuracy_threshold > 1):
-                raise ValueError()
-        except ValueError:
-            print(f"Invalid input: {acc}. Changing to default threshold (=1)")
+        if acc and not acc.isspace():
+            try:
+                accuracy_threshold = float(acc)
+                if(accuracy_threshold < 0 or accuracy_threshold > 1):
+                    raise ValueError()
+            except ValueError:
+                print(f"Invalid input: {acc}. Changing to default threshold (=1)")
+                accuracy_threshold = 1
+        else:
             accuracy_threshold = 1
         
-        tm = input("Now select a timeout in seconds: ")
+        tm = input("(Optional) Select a timeout in seconds: ")
+        if tm and not tm.isspace():
+            try:
+                timeout_seconds = int(tm)
+            except ValueError:
+                print(f"Invalid input: {tm}. Changing to default timeout (=0 s)")
+                timeout_seconds = 0
+        else:
+            timeout_seconds = 0
+    else:
+        global parser
+        parser.add_argument('source', help="The source node")
+        parser.add_argument('target', help="The target node")
+        parser.add_argument('dataset', choices=["DBPEDIA", "WIKIDATA", "YAGO"], help="The dataset you want to use")
+        parser.add_argument('algorithm', choices=["white-rabbit", "query-expansion", "embedding", "llm"], help="The algorithm you want to test")
+        parser.add_argument('-e', '--embedding', required=False, default=None, choices=[str(e.name) for e in EmbeddingType], help="The embedding you want to use. Only works if you chose 'embedding' as an algorithm")
+        parser.add_argument('-t', '--timeout', required=False, default=0, help="The optional timeout in seconds. If 0, no timeout is applied")
+        parser.add_argument('-a', '--accuracy-threshold', required=False, default=1, help="The optional accuracy threshold accepted. Must be a float in (0, 1]")
+
+        arguments = parser.parse_args()
+        source: str = arguments.source
+        target: str = arguments.target
+        dataset = DATASETS[arguments.dataset.upper()]
+        alg: str = arguments.algorithm
+        embedding = arguments.embedding
+        if(alg.lower() in ["wr", "white-rabbit"]):
+            algorithm = dataset.white_rabbit
+        elif(alg.lower() in ["qe", "query-expansion"]):
+            algorithm = dataset.query_expansion
+        elif(alg.lower() == "embedding"):
+            algorithm = dataset.embedding
+            if not embedding:
+                print("'embedding' algorithm requires argument '-e', '--embedding'")
+                return
+            embedding = EmbeddingType[embedding]
+        elif(alg.lower() == "llm"):
+            algorithm = dataset.llm
+
+        acc = arguments.accuracy_threshold
+        try:
+            accuracy_threshold = float(acc)
+            if(accuracy_threshold <= 0 or accuracy_threshold > 1):
+                raise ValueError()
+        except ValueError:
+            print(f"Invalid accuracy: {acc}. Must be a float in (0, 1]")
+            return
+        
+        tm = arguments.timeout 
         try:
             timeout_seconds = int(tm)
         except ValueError:
-            print(f"Invalid input: {tm}. Changing to default timeout (=360 s)")
-            timeout_seconds = 360
-    else:
-        try:
-            source = arguments[1]
-            target = arguments[2]
-            try:    
-                dataset = DATASETS[arguments[3].upper()]
-            except KeyError as e:
-                print(f"Invalid parameter for dataset: {e}")
-                return
-
-            alg = arguments[4]
-            algorithm = None
-            embedding = None
-            if(alg.upper() in ["1", "The White Rabbit"]):
-                algorithm = dataset.white_rabbit
-            elif(alg.upper() in ["2", "Query Expansion"]):
-                algorithm = dataset.query_expansion
-            elif(alg.upper() in ["3", "Embeddings"]):
-                algorithm = dataset.embedding
-                embedding = EmbeddingType(arguments[5])
-            elif(alg.upper() in ["4", "LLM"]):
-                algorithm = dataset.llm
-            else:
-                print(f"Invalid algorithm: {alg}")
-                return
-            
-        except IndexError:
-            usage()
-            return
+            print(f"Invalid timeout: {tm}. Changing to no timeout.")
+            timeout_seconds = 0
     inputs = (source, target, accuracy_threshold)
     print("Inputs: ")
     print(f"\tSource: {source}")
@@ -138,7 +150,12 @@ def main(arguments: list[str] = []):
     print(f"\tAccuracy Threshold: {accuracy_threshold}")
     print(f"\tTimeout in {timeout_seconds} seconds.")
     print("Starting the algorithm...")
-    time, length, pc, ta, path = timeout(algorithm, inputs, embedding_type=embedding or EmbeddingType.WIKI2VEC, timeout=timeout_seconds)
+    if timeout_seconds:
+        time, length, pc, ta, path = timeout(algorithm, inputs, embedding_type=embedding or EmbeddingType.WIKI2VEC, timeout=timeout_seconds)
+    else:
+        embedding_type=embedding or EmbeddingType.WIKI2VEC
+        model = load_model(embedding_type)
+        time, length, pc, ta, path = algorithm(model, *inputs)
     if not length:
         print("Algorithm timed out and/or no valid path was found. Try increasing the timeout.")
     else:
@@ -153,8 +170,4 @@ def main(arguments: list[str] = []):
 
 
 if __name__ == "__main__":
-    print(len(argv))
-    if len(argv) < 2:
-        main()
-    else:
-        main(argv)
+    main()
